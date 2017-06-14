62f76dbfea4a1eb8fcca63559186574c18b3b846
Anthony <hoangngochai@gmail.com>
2017-06-14 11:24:06 +0700

    - Show home page with style

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
-// app.use(express.static('public'));
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use('/static',express.static('public'));
 app.listen(3000, () => console.log('Server started'))
-app.get('/', (req, res) => res.send('home'));
+app.get('/', (req, res) => res.render('home'));
 	
